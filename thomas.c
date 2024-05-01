acbc3d1d475fda24c69bd2e111b5863c5f1edfe0
ange <150468696+blurosies@users.noreply.github.com>
2024-05-01 17:04:42 +0200

    Update thomas.c

--- thomas.c
+++ thomas.c
@@ -13,81 +13,78 @@ void gotoxy(int x, int y) {
 
 void affiche_vache_l(int x,int y)
 {
-    gotoxy(x,y);
-    printf("  __\n");
-    gotoxy(x+1,y);
-    printf(" <  \\ \n");
-    gotoxy(x+2,y);
-    printf("  ----\n");
-    gotoxy(x+3,y);
-    printf("        \\   ^__^\n");
-    gotoxy(x+4,y);
-    printf("         \\  (o-)\\_______\n");
-    gotoxy(x+5,y);
-    printf("            (__)\\       )\\/\\\n");
+    update();
     gotoxy(x+6,y);
+    printf("            ^__^\n");
+    gotoxy(x+7,y);
+    printf("            (o-)\\_______\n");
+    gotoxy(x+8,y);
+    printf("            (__)\\       )\\/\\\n");
+    gotoxy(x+9,y);
     printf("             U  ||----w |\n");
-    gotoxy(x+7,y);
+    gotoxy(x+10,y);
     printf("                ||     ||\n");
+    gotoxy(x+11,y);
+    printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+    sleep (1);
+    
 }
 
 
 
-void affiche_vache(int x, int y){
-    gotoxy(x,y);
-    printf("  __\n");
-    gotoxy(x+1,y);
-    printf(" <  \\ \n");
-    gotoxy(x+2,y);
-    printf("  ----\n");
-    gotoxy(x+3,y);
-    printf("        \\   ^__^\n");
-    gotoxy(x+4,y);
+void affiche_vache(int x, int y,int z){
+
+    update();
+    gotoxy(x+z-8,y);
+    printf("   ----\n");
+    gotoxy(x+z-7,y);
+    printf(" < mooo > \n");
+    gotoxy(x+z-6,y);
+    printf("   ----\n");
+    gotoxy(x+z-5,y);
+    printf("                  /\\ /\\\n");
+    gotoxy(x+z-4,y);
+    printf("        \\   ^__^ /  \\  \\\n");
+    gotoxy(x+z-3,y);
     printf("         \\  (oo)\\_______\n");
-    gotoxy(x+5,y);
+    gotoxy(x+z-2,y);
     printf("            (__)\\       )\\/\\\n");
-    gotoxy(x+6,y);
-    printf("                ||----w |\n");
-    gotoxy(x+7,y);
-    printf("                ||     ||\n");
-    
+    gotoxy(x+z-1,y);
+    printf("                \\\\----w \\\n");
+    gotoxy(x+z,y);
+    printf("                 \\\\     \\\\\n");
+    gotoxy(x+11,y);
+    printf("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+    usleep (150000);
     
 }
+
+
 
 int main ()
-{       
-        update();
-        affiche_vache(10,20);
-        usleep (150000);
-        update();
-        affiche_vache(10,19);
-        usleep (150000);
-        update();
-        affiche_vache(10,18);
-        usleep (150000);
-        update();
-        affiche_vache(10,17);
-        usleep (150000);
-        update();
-        affiche_vache(10,16);
-        usleep (150000);
-        update();
+{         
+        affiche_vache(10,20,8);
+    
+        affiche_vache(10,19,8);
+        
+        affiche_vache(10,18,8);
+        
+        affiche_vache(10,17,8);
+        
+        affiche_vache(10,16,9);
+        
         affiche_vache_l(10,15);
-        sleep (1);;
-        update();
-        affiche_vache(10,14);
-        usleep (150000);
-        update();
-        affiche_vache(10,13);
-        usleep (150000);
-        update();
-        affiche_vache(10,12);
-        usleep (150000);
-        update();
-        affiche_vache(10,11);
-        usleep (150000);
-        update();
-        affiche_vache(10,10);
-        usleep (150000);
-        update();
+        
+        affiche_vache(10,14,9);
+        
+        affiche_vache(10,13,8);
+        
+        affiche_vache(10,12,8);
+        
+        affiche_vache(10,11,8);
+        
+        affiche_vache(10,10,8);
+        
 }
+
+
